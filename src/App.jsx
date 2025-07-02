262170de961bde6b557ba7e8f1c04b1f07120b0e
TkWarrior <talibulhaque369@gmail.com>
2025-07-03 00:35:47 +0530

    added routes for important page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router'
 import './App.css'
 import Home from './page/Home/Home'
 import Archive from './page/Archive/Archive';
+import Important from './page/Important/Important';
 
 function App() {
  
@@ -10,7 +11,8 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/archive" element={<Archive/>}></Route>
+        <Route path="/archive" element={<Archive />}></Route>
+        <Route path="/important" element={<Important/>}></Route>
       </Routes>
     </>
   );
